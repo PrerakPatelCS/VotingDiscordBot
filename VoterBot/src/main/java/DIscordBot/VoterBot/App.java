cc2b07a11ed168226fda84ad9a7d37ec6cd6bce0
PrerakPatelCS <patelprerak121@gmail.com>
2021-03-27 16:29:11 -0400

    new commit

--- VoterBot/src/main/java/DIscordBot/VoterBot/App.java
+++ VoterBot/src/main/java/DIscordBot/VoterBot/App.java
@@ -14,7 +14,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class App extends ListenerAdapter {
     public static void main( String[] args ) throws LoginException
     {
-        JDABuilder.createLight("ODI1NDIwNTMwMjEwNzAxMzEy.YF9qxQ.pnAZD0MCP9lLEWS6nGFxajNVQTw", GatewayIntent.GUILD_MESSAGES,
+        JDABuilder.createLight("token", GatewayIntent.GUILD_MESSAGES,
             GatewayIntent.DIRECT_MESSAGES)
             .addEventListeners(new Event()).setActivity(Activity.playing("Candidate Voting"))
             .build();
